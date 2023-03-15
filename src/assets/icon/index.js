3f7de41f3a340f89de1d764543cf0ef011969a02
Yang-ah <yangah0409@gmail.com>
2023-03-15 16:20:39 +0900

    added icons svg

---
+++ src/assets/icon/index.js
@@ -0,0 +1,45 @@
+// 비어있는 체크박스
+export { ReactComponent as SquareIcon } from "./square.svg";
+export { ReactComponent as SquareCheckIcon } from "./square-check.svg";
+
+// 채워져있는 체크박스
+export { ReactComponent as SolidSquareIcon } from "./solid-square.svg";
+export { ReactComponent as SolidSquareCheckIcon } from "./solid-square-check.svg";
+
+// 돋보기 (search)
+export { ReactComponent as SearchIcon } from "./magnifying-glass.svg";
+
+// 별
+export { ReactComponent as SolidStarIcon } from "./solid-star.svg";
+export { ReactComponent as StarIcon } from "./star.svg";
+
+// 반별
+export { ReactComponent as StarHalfStrokeIcon } from "./star-half-stroke.svg";
+export { ReactComponent as SolidStarHalfIcon } from "./solid-star-half.svg";
+
+// 공감, 비공감
+export { ReactComponent as ThumbsUpIcon } from "./thumbs-up.svg";
+export { ReactComponent as ThumbsDownIcon } from "./thumbs-down.svg";
+
+// 좋아요
+export { ReactComponent as HeartIcon } from "./heart.svg";
+export { ReactComponent as SolidHeartIcon } from "./solid-heart.svg";
+
+// 북마크
+export { ReactComponent as SolidBookmarkIcon } from "./solid-bookmark.svg";
+
+// chevron
+export { ReactComponent as ChevronLeftIcon } from "./chevron-left.svg";
+export { ReactComponent as ChevronRightIcon } from "./chevron-right.svg";
+
+// angles-right  (더보기)
+export { ReactComponent as DoubleChevronRightIcon } from "./angles-right.svg";
+
+// 설정
+export { ReactComponent as SettingIcon } from "./solid-gear.svg";
+
+// 휴지통
+export { ReactComponent as TrashIcon } from "./trash-can.svg";
+
+// 수정
+export { ReactComponent as ModifyIcon } from "./solid-pen-to-square.svg";
