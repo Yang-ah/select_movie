8ff05581f9edf939c486a5375bbf061f964e5649
AbcLabFeShin <tlswnd3127@gmail.com>
2023-03-23 16:05:40 +0900

    중간저장

--- src/components/Common/Modal/index.js
+++ src/components/Common/Modal/index.js
@@ -3,17 +3,15 @@ import styles from './modal.module.scss';
 import Button from '../Button';
 
 function Modal({ setModalOpen, id, title, content, writer }) {
-    // 모달 끄기 
+
     const closeModal = () => {
         setModalOpen(false);
     };
 
-    // 모달 외부 클릭시 끄기 처리
     // Modal 창을 useRef로 취득
     const modalRef = useRef(null);
     
     useEffect(() => {
-        // 이벤트 핸들러 함수
         const handler = (event) => {
             // mousedown 이벤트가 발생한 영역이 모달창이 아닐 때, 모달창 제거 처리
             if (modalRef.current && !modalRef.current.contains(event.target)) {
@@ -23,12 +21,10 @@ function Modal({ setModalOpen, id, title, content, writer }) {
         
         // 이벤트 핸들러 등록
         document.addEventListener('mousedown', handler);
-        // document.addEventListener('touchstart', handler); // 모바일 대응
         
         return () => {
             // 이벤트 핸들러 해제
             document.removeEventListener('mousedown', handler);
-            // document.removeEventListener('touchstart', handler); // 모바일 대응
         };
     });
 
