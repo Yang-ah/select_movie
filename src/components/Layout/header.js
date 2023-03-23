2f6b15050f59b911a5020a8d04aea4ff1fb88278
Yang-ah <yangah0409@gmail.com>
2023-03-23 11:58:42 +0900

    feature(Layout:header) : modified size, feature(fonts):seperated fonts to style directory

--- src/components/Layout/header.js
+++ src/components/Layout/header.js
@@ -2,22 +2,23 @@ import React from "react";
 import styles from "./header.module.scss";
 
 import { Link } from "react-router-dom";
+import { Button } from "../Common";
 
 // 인라인 스타일링 지양! 유지보수 어렵.
 const Header = () => {
   return (
-    <section className={styles.headerBody}>
-      <Link to="/" className={styles.Link}>
-        <div className={styles.headerLogo}>무비셀렉터</div>
+    <header className={styles.wrap}>
+      <Link to="/">
+        <div className={styles.logo}>무비셀렉터</div>
       </Link>
-      {/* <input type="text"  /> */}
-      <div>검색</div>
-      <div className={styles.headerLogin}>
-        <Link to="/auth/login" className={styles.Link}>
-          <p className={styles.headerSign}>로그인</p>
+
+      <div className={styles.right}>
+        <p>TODO : searchInput</p>
+        <Link to="/auth/login">
+          <Button children={"로그인"} className={styles.headerSign} />
         </Link>
       </div>
-    </section>
+    </header>
   );
 };
 
