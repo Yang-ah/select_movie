1ae4d6e8cb48b64d8ada677d93f297b8035d5f57
Yang-ah <yangah0409@gmail.com>
2023-03-28 13:45:26 +0900

    feat(comment):modified structure

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,14 +1,8 @@
 import styles from "./comment.module.scss";
 import cx from "classnames";
-import {
-  ShareIcon,
-  SolidStarIcon,
-  ThumbsUpIcon,
-  ThumbsDownIcon,
-} from "../../assets/icon";
 import Button from "../Common/Button";
 
-import Stars from "../Common/Stars";
+import { HeaderLeft, HeaderRight } from "./_shared";
 
 const Comment = ({
   comment,
@@ -20,67 +14,25 @@ const Comment = ({
   down = 0,
   className,
 }) => {
+  const isCommentInput = type === "commentInput";
+
   return (
-    <article className={cx(styles.wrap, styles[type], className)}>
+    <section className={cx(styles.wrap, styles[type], className)}>
       <header>
-        <div className={styles.left}>
-          {type === "child" && <ShareIcon className={styles.IShare} />}
-          <p className={styles.profileIcon}>🤔</p>
-
-          <div className={styles.profileText}>
-            <h2 className={styles.userName}>{userName}</h2>
-            <p className={styles.date}>{date}</p>
-          </div>
-        </div>
-
-        <div
-          className={cx(
-            styles.right,
-            type === "commentInput" && styles.starsWrap
-          )}
-        >
-          {type === "commentInput" ? (
-            <Stars className={styles.stars} />
-          ) : (
-            <>
-              {type === "child" || (
-                <p className={styles.rating}>
-                  <SolidStarIcon />
-                  {rating}
-                </p>
-              )}
-              {type === "preview" || (
-                <>
-                  <span className={styles.upDown}>
-                    <button name="up">
-                      <ThumbsUpIcon />
-                      {up + ""}
-                    </button>
-                    <button name="down">
-                      <ThumbsDownIcon />
-                      {down + ""}
-                    </button>
-                  </span>
-                  <button type="button" name="report">
-                    신고하기
-                  </button>
-                </>
-              )}
-            </>
-          )}
-        </div>
+        <HeaderLeft type={type} userName={userName} date={date} />
+        <HeaderRight type={type} rating={rating} up={up} down={down} />
       </header>
+
       <main className={styles.comment}>
-        {type === "commentInput" ? (
+        {isCommentInput && (
           <>
             <textarea className={styles.inputWrap} />
             <Button option="comment" children="코멘트등록" />
           </>
-        ) : (
-          comment
         )}
+        {isCommentInput || comment}
       </main>
-    </article>
+    </section>
   );
 };
 
