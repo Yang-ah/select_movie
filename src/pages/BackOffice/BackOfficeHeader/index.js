44cd4fd78953935c8d6050f6338ff2b12fe9efa4
AbcLabFeShin <tlswnd3127@gmail.com>
2023-03-31 12:46:17 +0900

    관리자 로그인 모달

--- src/pages/BackOffice/BackOfficeHeader/index.js
+++ src/pages/BackOffice/BackOfficeHeader/index.js
@@ -1,11 +1,29 @@
 import Button from "../../../components/Common/Button";
 import styles from "./backOfficeHeader.module.scss";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import  CmsModal  from '../CMS/cmsModal';
 
 const BackOfficeHeader = ({ path }) => {
+  const navigate = useNavigate();
+  const moveMovies = () => { navigate(`/backoffice/movies`); };
+  const moveUsers = () => { navigate(`/backoffice/users`); };
+  const moveReviews = () => { navigate(`/backoffice/reviews`); };
+  
+   // 모달창 노출 여부 state
+   const [modalOpen, setModalOpen] = useState(false);
+   // 모달창 노출
+   const showModal = () => { setModalOpen(true); };
+
   return (
     <>
       <header className={styles.header}>
-        <h1>관리자 페이지</h1>
+        <h1 onClick={showModal}>관리자 페이지</h1>
+        <CmsModal
+            modalOpen1={modalOpen}
+            setModalOpen={setModalOpen}
+            children='1' 
+            buttonChildren='2' />
         <div className={styles.today}>
           <p>오늘</p>
           <p>
@@ -14,21 +32,26 @@ const BackOfficeHeader = ({ path }) => {
         </div>
       </header>
       <nav className={styles.nav}>
-        <Button option="secondary" className={styles.button}>
+        
+        <Button option="secondary" className={styles.button}
+        onClick={moveMovies}>
           <h3>등록된 영화</h3>
           <h1>
             <span>65 </span>개
           </h1>
           <h2>영화관리</h2>
         </Button>
-        <Button option="secondary" className={styles.button}>
+    
+        <Button option="secondary" className={styles.button}
+        onClick={moveUsers}>
           <h3>등록된 리뷰</h3>
           <h1>
             <span>65 </span>개
           </h1>
           <h2>회원관리</h2>
         </Button>
-        <Button option="secondary" className={styles.button}>
+        <Button option="secondary" className={styles.button}
+        onClick={moveReviews}>
           <h3>전체 리뷰</h3>
           <h1>
             <span>65 </span>개
