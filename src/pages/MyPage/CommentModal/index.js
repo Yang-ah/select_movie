65c699c83c040df638a702cb6b3ce9f970d9d571
rntjdal <rntjdal98@gmail.com>
2023-04-03 08:26:16 +0900

    fix: my

--- src/pages/MyPage/CommentModal/index.js
+++ src/pages/MyPage/CommentModal/index.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Link } from "react-router-dom";
 import { CSSTransition } from "react-transition-group";
 import styles from "./modal.module.scss";
 import { Button, Input } from "../../../components";
 
-const InfoModal = ({
+export const FixModal = ({
   modalOpen1,
   setModalOpen,
   children,
@@ -57,17 +56,24 @@ const InfoModal = ({
       unmountOnExit
     >
       <div className={styles.overlay}>
-        <section ref={modalRef} className={styles.container}>
-          <header className={styles.title}>리뷰 수정</header>
-          <Input className={styles.content} onChange={onChange} value={text} />
-          <footer className={styles.buttonBox}>
-            <Button className={styles.cancelButton} onClick={closeModal}>
+        <section ref={modalRef} className={styles.containerFix}>
+          <header className={styles.titleFix}>리뷰 수정</header>
+          <Input
+            className={styles.contentFix}
+            onChange={onChange}
+            value={text}
+          />
+          <footer className={styles.buttonBoxFix}>
+            <Button className={styles.cancelButtonFix} onClick={closeModal}>
               취소
             </Button>
-            <Button className={styles.resetButton} onClick={onReset}>
+            <Button className={styles.resetButtonFix} onClick={onReset}>
               초기화
             </Button>
-            <Button className={styles.deleteButton} children={buttonChildren} />
+            <Button
+              className={styles.deleteButtonFix}
+              children={buttonChildren}
+            />
           </footer>
         </section>
       </div>
@@ -75,4 +81,66 @@ const InfoModal = ({
   );
 };
 
-export default InfoModal;
+export const DeleteModal = ({
+  modalOpen1,
+  setModalOpen,
+  children,
+  notion,
+  content,
+  buttonChildren,
+  ...props
+}) => {
+  const modalRef = useRef(null);
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  useEffect(() => {
+    const handler = (event) => {
+      // mousedown 이벤트가 발생한 영역이 모달창이 아닐 때, 모달창 제거 처리
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setModalOpen(false);
+      }
+    };
+    // 이벤트 핸들러 등록
+    document.addEventListener("mousedown", handler);
+    return () => {
+      // 이벤트 핸들러 해제
+      document.removeEventListener("mousedown", handler);
+    };
+  });
+  //console.log(`컴포넌트 모달오픈${modalOpen1}`)
+  //if(modalOpen1===false){return null}
+
+  return (
+    <CSSTransition
+      in={modalOpen1}
+      timeout={300}
+      classNames={{
+        enterActive: styles.modalEnterActive,
+        enterDone: styles.modalEnterDone,
+        exitActive: styles.modalExit,
+        exitDone: styles.modalExitActive,
+      }}
+      unmountOnExit
+    >
+      <div className={styles.overlay}>
+        <section ref={modalRef} className={styles.containerDelete}>
+          <header className={styles.titleDelete}>리뷰 삭제</header>
+          <main className={styles.contentDelete}>
+            리뷰를 삭제 하시겠습니까?
+          </main>
+          <footer className={styles.buttonBoxDelete}>
+            <Button className={styles.cancelButtonDelete} onClick={closeModal}>
+              취소
+            </Button>
+            <Button
+              className={styles.deleteButtonDelete}
+              children={buttonChildren}
+            />
+          </footer>
+        </section>
+      </div>
+    </CSSTransition>
+  );
+};
