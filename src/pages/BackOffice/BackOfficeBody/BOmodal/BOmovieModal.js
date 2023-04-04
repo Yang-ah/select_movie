7c2f7049303d8c2da2c0bb5e8b1a935a03429074
myUBT <tlswnd3127@gmail.com>
2023-04-04 16:35:13 +0900

    a

--- src/pages/BackOffice/BackOfficeBody/BOmodal/BOmovieModal.js
+++ src/pages/BackOffice/BackOfficeBody/BOmodal/BOmovieModal.js
@@ -16,10 +16,11 @@
   "actors": "string[] | null"
 }
 */
-import { useEffect, useRef } from "react";
-import styles from "./modal.module.scss";
+import React, { useState, useEffect, useRef } from "react";
+import { Button, Input } from "../../../../components";
+import styles from "./BOmovieModal.module.scss";
 import { CSSTransition } from "react-transition-group";
-import Button from "../Button";
+
 import cx from "classnames";
 
 const BOmovieModal = ({
@@ -30,8 +31,10 @@ const BOmovieModal = ({
   modalOpen1,
   setModalOpen,
   onClick,
+  num,
   ...props
 }) => {
+
   // Modal 창을 useRef로 취득
   const modalRef = useRef(null);
 
@@ -53,43 +56,127 @@ const BOmovieModal = ({
       document.removeEventListener("mousedown", handler);
     };
   });
-  //console.log(`컴포넌트 모달오픈${modalOpen1}`)
-  //if(modalOpen1===false){return null}
-  return (
-    <CSSTransition
-      in={modalOpen1}
-      timeout={300}
-      classNames={{
-        enterActive: styles.modalEnterActive,
-        enterDone: styles.modalEnterDone,
-        exitActive: styles.modalExit,
-        exitDone: styles.modalExitActive,
-      }}
-      unmountOnExit
-    >
-      <div className={styles.overlay}>
-        <section
-          ref={modalRef}
-          className={cx(styles.container, className, styles[option])}
-        >
-          <header className={styles.title}>안내</header>
-          <div className={styles.content}>{children}</div>
-          <footer className={styles.buttonBox}>
-            <Button
-              className={styles.cancelButton}
-              children={"취소"}
-              onClick={closeModal}
-            />
-            <Button
-              className={styles.deleteButton}
-              children={buttonChildren}
-              onClick={onClick}
-            />
-          </footer>
-        </section>
-      </div>
-    </CSSTransition>
+
+  const [form, setForm] = useState(
+    {
+      title: "title",
+      plot: "plot",
+      releasedAt: "releasedAt",
+      rating: "rating",
+      runtime: "runtime",
+      company: "company",
+      genres: "genres",
+      staffs: [
+        {
+          name: "string",
+          role: "string"
+        }
+      ],
+      actors: "actors"
+    }
   );
+
+  const onChange = (e) => {
+    const { name, value } = e.currentTarget;
+    setForm({ ...form, [name]: value });
+  };
+
+  const onSubmit = async (e) => {
+    //NOTE: 새로고침 방지
+    e.preventDefault();
+  };
+
+return (
+<CSSTransition
+  in={modalOpen1}
+  timeout={300}
+  classNames={{
+    enterActive: styles.modalEnterActive,
+    enterDone: styles.modalEnterDone,
+    exitActive: styles.modalExit,
+    exitDone: styles.modalExitActive,
+  }}
+  unmountOnExit
+>
+  <div className={styles.overlay}>
+    <section
+      ref={modalRef}
+      className={cx(styles.container, className, styles[option])}
+    >
+      <header className={styles.title}>안내</header>
+      <div className={styles.content}>
+        <ul>
+        <li>ID {num}</li>
+        <Input
+            className={styles.inputClass}
+            label="영화제목"
+            onChange={onChange}
+            name="title"
+            value={form.title}
+          />
+        <Input
+            className={styles.inputClass}
+            label="줄거리"
+            onChange={onChange}
+            name="plot"
+            value={form.plot}
+          />
+        <Input
+            className={styles.inputClass}
+            label="상영일"
+            onChange={onChange}
+            name="releasedAt"
+            value={form.releasedAt}
+          />
+          <Input
+            className={styles.inputClass}
+            label="평점"
+            onChange={onChange}
+            name="rating"
+            value={form.rating}
+          />
+          <Input
+            className={styles.inputClass}
+            label='상영시간'
+            onChange={onChange}
+            name="runtime"
+            value={form.runtime}
+          />
+          <Input
+            className={styles.inputClass}
+            label='제작사'
+            onChange={onChange}
+            name="company"
+            value={form.company}
+          />
+          <Input
+            className={styles.inputClass}
+            label='genres'
+            onChange={onChange}
+            name="genres"
+            value={form.genres}
+          />
+        </ul>
+        
+
+      </div>
+      
+      <footer className={styles.buttonBox}>
+        <Button
+          className={styles.cancelButton}
+          children={"취소"}
+          onClick={closeModal}
+        />
+        <Button
+          className={styles.deleteButton}
+          children={buttonChildren}
+          onClick={onClick}
+        />
+      </footer>
+    </section>
+  </div>
+</CSSTransition>
+);
 };
 
 export default BOmovieModal;
