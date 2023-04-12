e25ea9a927bf74b3e59676ed26e2930c0ce0db8f
AbcLabFeShin <tlswnd3127@gmail.com>
2023-04-12 16:59:42 +0900

    a

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -22,13 +22,13 @@ const Home = () => {
         </div>
       </article>
       <article className={styles.category}>
-       <h2>🏊 유산소가 필요할땐 ! 액션영화</h2>
+        <h2>유산소가 필요할땐 ! 액션영화</h2>
         <HomeCarousel GenreId = 'fc84777a-d713-4539-a5b9-8c24f0c85b99' />
         <h2>남의 연애라도 봐 ! 로맨스영화</h2>
         <HomeCarousel GenreId = '73fa7e1d-0e3e-4506-9432-21c29faa8dd7' />
-        <h2>❌ 따라하지마 ! 범죄영화</h2>
+        <h2>따라하지마 ! 범죄영화</h2>
         <HomeCarousel GenreId = '801c5056-0479-415c-b205-9daecad91b0e' />
-        <h2>😆 외로울땐 ! 가족영화</h2>
+        <h2>외로울땐 ! 가족영화</h2>
         <HomeCarousel GenreId = '1c9e16ec-920f-4975-b028-b4c681084f88' />
       </article>
     </section>
