ab98faeb3e6a5f9a90b84855c83d4338c308200d
kyoongdev <9898junjun2@gmail.com>
2023-04-14 09:35:13 +0900

    feedback : 2023.04.14

--- src/pages/BackOffice/BackOfficeBody/BOpageNation/BOpageNation.js
+++ src/pages/BackOffice/BackOfficeBody/BOpageNation/BOpageNation.js
@@ -1,96 +1,108 @@
-import React, { useState } from "react";
-import { ChevronLeftIcon, ChevronRightIcon, DoubleChevronRightIcon } from "../../../../assets/icon";
-import styles from "./BOpageNation.module.scss";
-import {range} from 'lodash-es';
-import cx from "classnames";
-import { number } from "prop-types";
-const BOpageNation =({ pageNationNumber, setPageNumber, pageNumber, SearchPageNumber})=>{
-    const maxNum = 10;
-    const lastPage = Math.ceil(pageNationNumber/maxNum);
-    //console.log(lastPage);
-    const [navNum, setNavNum] =useState({
-        start : 1,
-        end : maxNum,
-    });
-    const pageUp = () => {
-        if (pageNumber < pageNationNumber) {
-          setPageNumber(pageNumber + 1);
-          if(pageNumber%maxNum ===0){
-            navUp();
-          }
-        }
-      };
-      const pageDown = () => {
-        if (pageNumber > 1) {
-          setPageNumber(pageNumber - 1);
-          if(pageNumber%maxNum ===1){
-            navDown();
-            setPageNumber(navNum.start  -1);
-          }
-        }
-      };
-    const navUp = () => {
-        if (navNum.end < pageNationNumber) {
-            setNavNum({
-                start : navNum.start + maxNum,
-                end : navNum.end + maxNum,
-            })
-            setPageNumber(navNum.start+maxNum);          
-        }
-      };
-      const navDown = () => {
-        if (navNum.start > 1) {
-            setNavNum({
-                start : navNum.start - maxNum,
-                end : navNum.end - maxNum,
-            })
-            setPageNumber(navNum.start - maxNum);
-        }
-      };
+import React, { useState } from 'react';
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  DoubleChevronRightIcon,
+} from '../../../../assets/icon';
+import styles from './BOpageNation.module.scss';
+import { range } from 'lodash-es';
+import cx from 'classnames';
+import { number } from 'prop-types';
+const BOpageNation = ({
+  pageNationNumber,
+  setPageNumber,
+  pageNumber,
+  SearchPageNumber,
+}) => {
+  const maxNum = 10;
+  const lastPage = Math.ceil(pageNationNumber / maxNum);
+  //console.log(lastPage);
+  const [navNum, setNavNum] = useState({
+    start: 1,
+    end: maxNum,
+  });
+  const pageUp = () => {
+    if (pageNumber < pageNationNumber) {
+      setPageNumber(pageNumber + 1);
+      if (pageNumber % maxNum === 0) {
+        navUp();
+      }
+    }
+  };
+  const pageDown = () => {
+    if (pageNumber > 1) {
+      setPageNumber(pageNumber - 1);
+      if (pageNumber % maxNum === 1) {
+        navDown();
+        setPageNumber(navNum.start - 1);
+      }
+    }
+  };
+  const navUp = () => {
+    if (navNum.end < pageNationNumber) {
+      setNavNum({
+        start: navNum.start + maxNum,
+        end: navNum.end + maxNum,
+      });
+      setPageNumber(navNum.start + maxNum);
+    }
+  };
+  const navDown = () => {
+    if (navNum.start > 1) {
+      setNavNum({
+        start: navNum.start - maxNum,
+        end: navNum.end - maxNum,
+      });
+      setPageNumber(navNum.start - maxNum);
+    }
+  };
 
-    return(
-
+  return (
     <ul className={styles.pagination}>
-        { pageNationNumber>maxNum+1 && navNum.start !== 1 &&
-        <li><DoubleChevronRightIcon className={styles.IconR} onClick={navDown}/></li>}
-
-        <li className={styles.prevIcon}>
-            <ChevronLeftIcon
-                className={styles.Icon}
-                onClick={pageDown}
-            />
+      {pageNationNumber > maxNum + 1 && navNum.start !== 1 && (
+        <li>
+          <DoubleChevronRightIcon className={styles.IconR} onClick={navDown} />
         </li>
+      )}
 
-        {pageNationNumber<navNum.end ?
-        range(navNum.start, pageNationNumber+1).map((number, i) => (
+      <li className={styles.prevIcon}>
+        <ChevronLeftIcon className={styles.Icon} onClick={pageDown} />
+      </li>
+
+      {pageNationNumber < navNum.end
+        ? range(navNum.start, pageNationNumber + 1).map((number, i) => (
             <li
-                value={i}
-                onClick={()=>setPageNumber(number)}
-                className={cx(styles.page, {[styles.currentPage]: number===pageNumber })}
+              value={i}
+              onClick={() => setPageNumber(number)}
+              className={cx(styles.page, {
+                [styles.currentPage]: number === pageNumber,
+              })}
             >
-                {number}
+              {number}
             </li>
-            ))
-        :range(navNum.start, navNum.end+1).map((number, i) => (
-        <li
-            value={i}
-            onClick={()=>setPageNumber(number)}
-            className={cx(styles.page, {[styles.currentPage]: number===pageNumber })}
-        >
-            {number}
-        </li>
-        ))}
+          ))
+        : range(navNum.start, navNum.end + 1).map((number, i) => (
+            <li
+              value={i}
+              onClick={() => setPageNumber(number)}
+              className={cx(styles.page, {
+                [styles.currentPage]: number === pageNumber,
+              })}
+            >
+              {number}
+            </li>
+          ))}
 
-        <li className={styles.nextIcon}>
-            <ChevronRightIcon
-                className={styles.Icon}
-                onClick={pageUp}
-            />
+      <li className={styles.nextIcon}>
+        <ChevronRightIcon className={styles.Icon} onClick={pageUp} />
+      </li>
+      {pageNationNumber > maxNum + 1 && navNum.end < pageNationNumber && (
+        <li>
+          <DoubleChevronRightIcon className={styles.Icon} onClick={navUp} />
         </li>
-        { pageNationNumber>maxNum+1 && navNum.end < pageNationNumber &&
-        <li ><DoubleChevronRightIcon className={styles.Icon} onClick={navUp}/></li>}
+      )}
     </ul>
-    )
-}
+  );
+};
 
 export default BOpageNation;
